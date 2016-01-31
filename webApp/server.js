07ad22619bc4427b531f1f8a8c3eaa955b979a31
GregChan <gregorywarrenchan@gmail.com>
2016-01-31 02:24:25 -0600

    Added real data to the browse.json and cleaned up some of the other pages

--- webApp/server.js
+++ webApp/server.js
@@ -31,9 +31,10 @@ app.get('/', function(req, res) {
             person: {
                 "name": "Lisa Eng",
                 "image": "https://media.licdn.com/media/AAEAAQAAAAAAAALfAAAAJDU2YWFiZGM0LTgxZmEtNDcyZC05ODI4LTViZGM1YTg5MDkyOQ.jpg",
-                "bio": "",
+                "work": ["Product Manager, Business Intelligence, Aereo", "Special Operations, Warby Parker Marketing, Quirky", "Special Customer Operations, Simon Schuster", "Associate, Triage Consulting Group"],
+                "education": ["MBA from NYU Stern Business School, University of California San Diego"],
                 "tags": ["strategy", "marketing", "tech", "SF", "49ers", "dogs", "consulting", "productmanager"],
-                "job": "Product at Oracle Data Cloud",
+                "job": "Product Manager at Oracle Data Cloud",
                 "quote": "I am a dog-lover, 49ers fan, and tech enthusiast. Previously in New York, I live in San Francisco now with my husband and malti-poo dog, Izzo. Yes named after Coach Izzo!"
             }
         });
@@ -45,16 +46,17 @@ app.get('/match', function(req, res) {
         user: {
             "name": "Student",
             "image": "http://d9hhrg4mnvzow.cloudfront.net/womensilab.com/coffeechat2/bb0185b8-sussana-shuman_07107207106x000002.jpg",
-            "bio": "",
+            "bio":"",
             "tags": ["tech", "sf", "49ers"],
             "job": "Northwestern University"
         },
         match: {
             "name": "Lisa Eng",
             "image": "https://media.licdn.com/media/AAEAAQAAAAAAAALfAAAAJDU2YWFiZGM0LTgxZmEtNDcyZC05ODI4LTViZGM1YTg5MDkyOQ.jpg",
-            "bio": "",
+            "work": ["Product Manager, Business Intelligence, Aereo", "Special Operations, Warby Parker Marketing, Quirky", "Special Customer Operations, Simon Schuster", "Associate, Triage Consulting Group"],
+                "education": ["MBA from NYU Stern Business School", "University of California San Diego"],
             "tags": ["strategy", "marketing", "tech", "SF", "49ers", "dogs", "consulting", "productmanager"],
-            "job": "Product at Oracle Data Cloud",
+            "job": "Product Manager at Oracle Data Cloud",
             "quote": "I am a dog-lover, 49ers fan, and tech enthusiast. Previously in New York, I live in San Francisco now with my husband and malti-poo dog, Izzo. Yes named after Coach Izzo!"
         }
     });
